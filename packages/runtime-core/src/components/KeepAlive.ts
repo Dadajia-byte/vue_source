97d948a8043f0eef560abb127743dca54386ce83
Dadajia-byte <1814650097@qq.com>
2024-12-11 23:34:01 +0800

    feat: 使用LRU缓存优化keepalive

--- packages/runtime-core/src/components/KeepAlive.ts
+++ packages/runtime-core/src/components/KeepAlive.ts
@@ -2,9 +2,15 @@ import { ShapeFlags } from "@vue/shared";
 import { onMounted, onUpdated } from "../apiLifecycle";
 import { getCurrentInstance } from "../component";
 
+
+
 export const KeepAlive = {
-    __isKeepAlive: true,
+  __isKeepAlive: true,
+  props:{
+    max:Number
+  },
   setup(props,{slots}) {
+    const {max} = props;
     const keys = new Set(); // 用来记录哪些组件缓存过
     const cache = new Map(); // 缓存表 <keep-alive key="xxx"></keep-alive>
     // 在这个组件中需要一些dom方法，可以将元素移动到一个div中
@@ -16,9 +22,35 @@ export const KeepAlive = {
       cache.set(pendingCacheKey, instance.subTree); // 缓存组件的虚拟节点，里面有组件的dom
     }
 
+    const {move,createElement,unmount:_unmount} = instance.ctx.renderer;
+    
+    function reset(vnode) { // 还原vnode上的标识
+      let {shapeFlag} = vnode;
+      if(shapeFlag & ShapeFlags.COMPONENT_SHOULD_KEEP_ALIVE) {
+        shapeFlag -= ShapeFlags.COMPONENT_SHOULD_KEEP_ALIVE;
+      } 
+      if(shapeFlag & ShapeFlags.COMPONENT_KEPT_ALIVE) {
+        shapeFlag -= ShapeFlags.COMPONENT_KEPT_ALIVE;
+      }
+      vnode.shapeFlag = shapeFlag;
+    }
+    function unmount(vnode) {
+      // 1. 先还原vnode上的标识
+      // 2. 然后再真卸载这个组件
+      reset(vnode);
+      _unmount(vnode);
+    }
+    function purneCacheEntry(key) {
+      const cached = cache.get(key);
+      // 还原vnode上的标识，否则无法移除逻辑
+      unmount(cached); // 走真删除dom元素
+      cache.delete(key);
+      keys.delete(key);
+    }
+
+
     // 这里是keepalive特有的初始化方法
     // 激活时执行
-    const {move,createElement} = instance.ctx.renderer;
     instance.ctx.activate = (vnode,container,anchor)=>{
       move(vnode,container,anchor);
     }
@@ -43,8 +75,14 @@ export const KeepAlive = {
       if (cacheVNode) {
         vnode.component = cacheVNode.component; // 不用重新创建组件实例，直接复用即可
         vnode.shapeFlag |= ShapeFlags.COMPONENT_KEPT_ALIVE; // 标记这个组件是keep-alive的, 不需要初始化了
+        keys.delete(key);
+        keys.add(key); // LRU缓存，借助js的Set数据结构
       } else {
-        keys.add(key)
+        keys.add(key);
+        if(max && keys.size > max) {
+          // 缓存已经满了
+          purneCacheEntry(keys.values().next().value);
+        }
       }
       vnode.shapeFlag |= ShapeFlags.COMPONENT_SHOULD_KEEP_ALIVE; // 这个组件不需要真的卸载
       return vnode; // 等待组件加载完毕后再去缓存
